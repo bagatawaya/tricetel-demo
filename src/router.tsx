f8bed6603f238ced716439166ba9bbb3d520431e
bagatawaya <138273936+bagatawaya@users.noreply.github.com>
2025-12-01 20:47:49 -0800

    Refactor router setup to use RouterProvider

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Wizard from "./pages/Wizard";
-import Help from "./pages/Help";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import "./styles.css";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Dashboard /> },
-  { path: "/wizard", element: <Wizard /> },
-  { path: "/help", element: <Help /> },
-]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
-export default router;
