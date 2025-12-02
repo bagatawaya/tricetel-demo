94c8b864a8fb92562bcc28bcfb23ae2a88bc854e
bagatawaya <138273936+bagatawaya@users.noreply.github.com>
2025-12-01 18:21:20 -0800

    Create Numbers component to display phone numbers

---
+++ src/pages/Numbers.tsx
@@ -0,0 +1,17 @@
+export default function Numbers() {
+  return (
+    <div>
+      <h1 className="page-title">Phone Numbers</h1>
+
+      <div className="card">
+        <h3>Current Number</h3>
+        <p>+1 (970) 772-6934</p>
+      </div>
+
+      <div className="card">
+        <h3>Purchase New Number</h3>
+        <button className="btn">Buy Number</button>
+      </div>
+    </div>
+  );
+}
