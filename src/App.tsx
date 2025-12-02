f0a789e3a121f2e9f30d0f69ecb24dd5a1a2cafe
bagatawaya <138273936+bagatawaya@users.noreply.github.com>
2025-12-01 18:15:37 -0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 40, fontSize: 24 }}>
+      <strong>Trice Demo App:</strong> UI is loading…
+    </div>
+  );
+}
